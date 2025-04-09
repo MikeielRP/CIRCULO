3268abe87e0f0bcad180491ce3c3325efc8c4baa
MikeielRP <72733304@continental.edu.pe>
2025-04-09 11:37:08 -0500

    segundo commit

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ class circulo:
         self.radio = radio
     def circunferencia(self):
         return 2 * self.PI * self.radio
+    def area(self):
+        return self.PI  *self.radio**2
 
 if __name__ == "__main__":
     c1=circulo(10)
     print(f"La circunferencia es {c1.circunferencia()}")
+    print(f"El area es {c1.area()}")
